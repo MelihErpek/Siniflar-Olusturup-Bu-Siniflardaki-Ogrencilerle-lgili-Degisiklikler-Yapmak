71c877d77ca81463b71305b510110e41f96c9357
MelihErpek <61746050+MelihErpek@users.noreply.github.com>
2020-04-02 16:35:19 +0300

    Update Dugum.hpp

--- include/Dugum.hpp
+++ include/Dugum.hpp
@@ -1,11 +1,4 @@
-/****************************************************************************
-**              		SAKARYA UNIVERSITY
-**              	BILGISAYAR MUHENDISLIGI BOLUMU
-**              		VERI YAPILARI
-**
-**              OGRENCI ISMI.....: Melih Erpek
-**              OGRENCI NUMARASI.: G171210393
-****************************************************************************/
+
 #ifndef  Dugum_hpp
 #define Dugum_hpp
 class Dugum // Dugum classi olusturulup gerekli degiskenler tanimlaniyor.
